86b665f4f1af45962d725886fb6f2b2d76788826
Frankdelos <43051918+Frankdelos@users.noreply.github.com>
2018-09-24 11:37:35 -0700

    Add files via upload

---
+++ LabAssignment2/main.ts
@@ -0,0 +1,21 @@
+
+
+class Greeter {
+  constructor(private first_name: string, private last_name: string) {
+  }
+  
+  showGreeting() {
+
+    const banner= document.getElementById('banner');
+    banner.innerHTML='Hello '+ this.first_name + ' '+ this.last_name + '.';
+  }
+
+  hideGreeting(){
+    const banner= document.getElementById('banner');
+    banner.innerHTML='r1c2';
+  
+  }
+  
+}
+  const greeter = new Greeter('Franklin', 'Delos Santos');
+
